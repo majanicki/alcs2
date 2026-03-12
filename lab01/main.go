5fb6e093b8650bd0aa6dd6667b61166da4611d2f
Maciej <maciej.ja@proton.me>
2026-03-12 17:36:22 +0100

    TODO: added more comments on what we need to do

--- lab01/main.go
+++ lab01/main.go
@@ -33,6 +33,7 @@ func printPermuation(permutation []int) {
 	fmt.Println("")
 }
 
+// Think about whether we want to make this more inline with what we discussed at labs
 func measureTime(f func()) (float64) {
 	start := time.Now()
 	f()
@@ -40,13 +41,40 @@ func measureTime(f func()) (float64) {
 	return elapsed.Seconds()
 }
 
+// We should read data with `make` allocations
+
+// Heuristic:
+// Prof: Average rows and columns and match lowest average with highest average 
+// Other: Pick closest with highest flow - this one is okay
+
+// for deltas we are changing two rows and two columns
+// deltas should be calculated in linear time
+
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	perm := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	randomPermutation(perm)
-	printPermuation(perm)
-	a, b := randomPair(len(perm))
-	fmt.Println(a, b)
-	elapsed := measureTime(func() {time.Sleep(2 * time.Second)})
-	fmt.Println(elapsed)
+	// perm := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// randomPermutation(perm)
+	// printPermuation(perm)
+	// a, b := randomPair(len(perm))
+	// fmt.Println(a, b)
+	// elapsed := measureTime(func() {time.Sleep(2 * time.Millisecond)})
+	// fmt.Println(elapsed)
+	// 2-OPT neighbourhoud
+	n := 5
+	// for randomizing the neighbourhood we need to start from random point
+	offset := random(n) // this offset is optional (should check if this correct)
+	for i := 0; i < n - 1; i++ { // skip unnecessary last iteration
+		for j := i + 1; j < n; j++ {
+			// calculate deltas
+			fmt.Println((i + offset) % n, (j + offset) % n)
+		}
+	}
+	// do the swap with xor if you want to be fast
+	// or do the regular swap but only for accepted neighbour
+
+	// outputing results:
+	// - quality, permutation, running time, how many jumps from neighbour to neighbours
+	//    how many times we evaluated delta
+	// for next week - loading data, heuristic, 2-opt neighbours
 }
